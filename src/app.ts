1b697724ffb4b979d2df65df5e2394257a3b0dc4
Ti Jhae <jhaeti@gmail.com>
2022-07-19 11:37:33 +0000

    Added code formatting

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,9 @@ app.use(morgan("dev"));
 
 // Route for testing
 app.get("/", (req, res) => {
-  res.send(
-    "<style>body{display:flex;justify-content:center;align-item:center;}h1{box-sizing:border-box; border-radius:1rem;width:80%;font-size:5rem;text-align:center;text-transform:uppercase;font-family:helvetica;background:rgba(200,200,200, 0.3); padding:4rem;}</style><h1>Welcome to my typscript boilerplate</h1>"
-  );
+    res.send(
+        "<style>body{display:flex;justify-content:center;align-item:center;}h1{box-sizing:border-box; border-radius:1rem;width:80%;font-size:5rem;text-align:center;text-transform:uppercase;font-family:helvetica;background:rgba(200,200,200, 0.3); padding:4rem;}</style><h1>Welcome to my typscript boilerplate</h1>"
+    );
 });
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const port = Number(process.env.PORT ?? 5000);
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Backend running on port ${port}`);
+    console.log(`Backend running on port ${port}`);
 });
