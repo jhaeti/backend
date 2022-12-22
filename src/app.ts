91d184698f5ce6b4a5d81b76777f5de55cc1b17b
Alhassan Tijani <jhaeti@gmail.com>
2022-12-22 17:34:56 +0000

    -Added json body parser middleware. -Added user route

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import cors from 'cors';
 
 import './db';
 
+// Api routes
+import UserApi from './routes/userRoute';
+
 const app = express();
+
+app.use(express.json());
 
 // Accept cross-origin-request
 app.use(
@@ -16,11 +21,7 @@ app.use(
 // Logging all request to the server console
 app.use(morgan('dev'));
 
-// Route for testing
-app.get('/', (req, res) => {
-    res.send(
-        '<style>body{display:flex;justify-content:center;align-item:center;}h1{box-sizing:border-box; border-radius:1rem;width:80%;font-size:5rem;text-align:center;text-transform:uppercase;font-family:helvetica;background:rgba(200,200,200, 0.3); padding:4rem;}</style><h1>Welcome to my typscript boilerplate</h1>',
-    );
-});
+// Using routes
+app.use(UserApi);
 
 export default app;
