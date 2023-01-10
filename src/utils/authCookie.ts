38f6be7f1aa96c5641f937a614ef72e8638ddc24
Alhassan Tijani <jhaeti@gmail.com>
2023-01-10 17:12:55 +0000

    for handling auth cookies

---
+++ src/utils/authCookie.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import TestError from '../errors/UserAuthError';
+
+// Get token from cookies
+// Recieve a cookieName and returns the token corresponding to that cookie
+export const getAuthToken = async (req: Request) => {
+    // Getting cookie from the req since it would be sent along with the request if present
+    const token: string = req.cookies[process.env.AUTH_COOKIE_NAME];
+    if (!token) throw new TestError('No token, authorisation deneid');
+    return token;
+};
+
+// Clearing cookie from the browser
+// Does not matter whether there exist a cookie with that name or not it simply clears cookies with the name
+export const clearAuthCookie = (res: Response) => {
+    res.clearCookie(process.env.AUTH_COOKIE_NAME);
+};
+
+// Store cookies in the browser for usage in future request
+export const setAuthCookie = (res: Response, token: string) => {
+    res.cookie(process.env.AUTH_COOKIE_NAME, token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+    });
+};
