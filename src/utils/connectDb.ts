63b6c885be19c1c68170f3aef98053c19541b6f3
Alhassan Tijani <jhaeti@gmail.com>
2023-02-02 10:30:00 +0000

    Handles connection to database

--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -2,34 +2,34 @@ import { ConnectOptions, connect } from 'mongoose';
 
 import User from '../models/User';
 
-const uri =
-    (process.env.NODE_ENV === 'test' && process.env.MONGO_TEST_URI) || process.env.MONGO_URI;
+const connectDb = (uri: string) => {
+    connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    } as ConnectOptions)
+        .then(() => {
+            console.info('Mongodb Connected...');
+        })
+        .catch(() => console.log('not connected'));
 
-console.log(uri);
-connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-} as ConnectOptions)
-    .then(async () => {
-        console.info('Mongodb Connected...');
-    })
-    .catch(() => console.log('not connected'));
+    (async function () {
+        const admin = await User.findOne({ role: 'admin' });
+        if (!admin) {
+            const {
+                DEFAULT_ADMIN_NAME: name,
+                DEFAULT_ADMIN_EMAIL: email,
+                DEFAULT_ADMIN_PASSWORD: password,
+            } = process.env;
+            const defaultAdminUser = new User({
+                name,
+                email,
+                password,
+                role: 'admin',
+            });
+            await defaultAdminUser.save();
+        }
+        console.log('Admin Present');
+    })();
+};
 
-(async function () {
-    const admin = await User.findOne({ role: 'admin' });
-    if (!admin) {
-        const {
-            DEFAULT_ADMIN_NAME: name,
-            DEFAULT_ADMIN_EMAIL: email,
-            DEFAULT_ADMIN_PASSWORD: password,
-        } = process.env;
-        const defaultAdminUser = new User({
-            name,
-            email,
-            password,
-            role: 'admin',
-        });
-        await defaultAdminUser.save();
-    }
-    console.log('Admin Present');
-})();
+export default connectDb;
